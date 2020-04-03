23e72bbe9e15a80ccfa6e54011ba6bfee488a669
Agere98 <44338036+Agere98@users.noreply.github.com>
2020-04-03 17:26:11 +0200

    Initial commit

---
+++ src/main/java/com/example/orbits/OrbitsApplication.java
@@ -0,0 +1,13 @@
+package com.example.orbits;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrbitsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrbitsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/orbits/OrbitsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.orbits;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrbitsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
